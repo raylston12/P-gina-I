8eb87d712c860162d110cb02221247128ab7a806
Raylston12 <raygames1928@gmail.com>
2020-08-17 19:30:44 -0230

    feature(img, audio)adicionei imagem e audio

---
+++ users.js
@@ -0,0 +1,16 @@
+var nome = window.prompt("qual seu nome?");         //pergunta o seu nome
+
+window.alert("Bem vindo " + nome);                  //pergunta o seu nome
+
+var idade = window.prompt("qual sua idade?");       //pergunta sua idade
+
+if (idade >= 18) {
+    alert("maior de idade")
+} else {                                           //sabe que você e maior de idade
+    alert("menor de idade")
+}
+
+function clicou(){                                //link do meu gitHub
+    window.open("https://github.com/raylston12");      
+}
+
