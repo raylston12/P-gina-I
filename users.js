2958b4fcef5161d4681ff2bbbb4dea0de2fa615b
Raylston12 <raygames1928@gmail.com>
2020-08-18 03:00:47 -0230

    feature(title, img, GitHub, Download, Discord,):adicionei um titulo h1, paragrafo, imagem, link do GitHub, Download do repositorio, e um link do Discord

--- users.js
+++ users.js
@@ -10,7 +10,16 @@ if (idade >= 18) {
     alert("menor de idade")
 }
 
-function clicou(){                                //link do meu gitHub
-    window.open("https://github.com/raylston12");      
+function clicou1(){                                //link do meu gitHub
+    window.open("https://github.com/raylston12/Pagina-I");      
 }
+
+function clicou2(){                                //link do meu gitHub
+    window.open("https://github.com/raylston12/Pagina-I/archive/master.zip");      
+}
+
+function clicou3(){                                //link do meu gitHub
+    window.open("https://discord.gg/mcjk8z");      
+}
+
 
