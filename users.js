6a8c37f6890eedf148a59eec524a66204310a987
Raylston12 <raygames1928@gmail.com>
2020-08-18 10:07:21 -0230

    mudei o titulo

--- users.js
+++ users.js
@@ -1,14 +1,3 @@
-var nome = window.prompt("qual seu nome?");         //pergunta o seu nome
-
-window.alert("Bem vindo " + nome);                  //pergunta o seu nome
-
-var idade = window.prompt("qual sua idade?");       //pergunta sua idade
-
-if (idade >= 18) {
-    alert("maior de idade")
-} else {                                           //sabe que você e maior de idade
-    alert("menor de idade")
-}
 
 function clicou1(){                                //link do meu gitHub
     window.open("https://github.com/raylston12/Pagina-I");      
